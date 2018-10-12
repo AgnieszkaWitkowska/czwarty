6913cb0b80cd79665b4894652223203b6b7e65f1
agawit <agnieszkakojro@wp.pl>
2018-10-12 15:58:07 +0200

    Wspol

---
+++ cmake-build-debug/CMakeFiles/Wspol.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Wspol.dir/main.c.o"
+  "Wspol.pdb"
+  "Wspol"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Wspol.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
